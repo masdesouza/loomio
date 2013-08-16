4790f96ededdc33e6251b451666db231b927605f
Robert Guthrie <rob@guthr.ie>
2013-08-16 14:45:38 +1200

    jsut before I probably screw up active admin

--- app/models/email.rb
+++ app/models/email.rb
@@ -9,4 +9,25 @@ class Email < ActiveRecord::Base
     email.reply_to = headers[:reply_to]
     email
   end
+
+  def substitute_placeholders(substitutions)
+    [:body, :subject].each do |text_name|
+      text = self.send(text_name)
+
+      /{{\S}}/.match(text).to_a.each do |match|
+
+        object, method = match.split('.')
+        replacement = if method.nil?
+          # assume it is a url helper call
+          Rails.application.routes.url_helpers.send(object)
+        else
+          substitutions[object].send(method)
+        end
+
+        text.gsub!("{{#{match}}}", replacement)
+      end
+
+      self[text_name] = text
+    end
+  end
 end
--- spec/models/email_spec.rb
+++ spec/models/email_spec.rb
@@ -2,9 +2,12 @@ require 'spec_helper'
 
 describe Email do
   describe 'substitute_placeholders' do
+    let(:user) { stub(:user, name: 'bill jones') }
+
     before do
       email.subject = "open this email please {{recipient.name}}"
       email.body = "hi {{recipient.name}} click this link {{root_url}}"
+      email.substitute_placeholders(recipient: user)
     end
 
     it "replaces object.method placeholders" do
